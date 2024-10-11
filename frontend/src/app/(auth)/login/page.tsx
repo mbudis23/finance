b771bce5455a114e5d97b7afa358a85595371476
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-10-11 14:30:37 +0700

    Feat: Login page in frontend

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "@/components/scoped/loginPage/loginCard";
+
+export default function LoginPage() {
+    return (
+        <main className="min-w-full min-h-screen bg-white text-black flex justify-center items-center text-[16px]">
+        <LoginCard/>
+        </main>
+    );
+}
